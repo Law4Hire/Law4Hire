e890ebb4e65a2fc5f525d3114578ea17de96e9e8
Law4Hire <USImmigrationHelp.Law4Hire@gmail.com>
2025-07-13 20:44:27 -0400

    All the information is there. Now, checkin gin the migration files.

---
+++ Law4Hire.API/Migrations/20250713215316_CorrectErrors3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Law4Hire.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrectErrors3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VisaTypeQuestions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    VisaTypeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    QuestionKey = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    QuestionText = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false),
+                    IsRequired = table.Column<bool>(type: "bit", nullable: false),
+                    ValidationRules = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VisaTypeQuestions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VisaTypeQuestions_VisaTypes_VisaTypeId",
+                        column: x => x.VisaTypeId,
+                        principalTable: "VisaTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VisaTypeQuestions_VisaTypeId",
+                table: "VisaTypeQuestions",
+                column: "VisaTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VisaTypeQuestions");
+        }
+    }
+}
